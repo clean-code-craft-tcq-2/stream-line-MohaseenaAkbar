ed43fcf48fdb75f5c6a761adc608416b89702b8a
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-04 15:42:41 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -11,12 +11,13 @@ def CelsiustoFarenheit(TempInCelsius):
 def temp_preprocess(rew_Temp_data):
   Temp_celsius=temp_A2D_conversion(rew_Temp_data)
   Temp_Farenheit=CelsiustoFarenheit(Temp_celsius)
-  return temp_Farenheit
+  return Temp_Farenheit
   
 def Sensor_reading_to_receiver():
   rew_Temp_data=get_Temp_sensor_data()
   Temp_Output=temp_preprocess(rew_Temp_data)
   print(Temp_Output)
-  
-Sensor_reading_to_receiver()
+
+if __name == '__main':
+  Sensor_reading_to_receiver()
   

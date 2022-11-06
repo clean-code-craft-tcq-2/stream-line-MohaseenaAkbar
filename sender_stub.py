09a62c720963507a5db39563828b03987754093b
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-07 00:08:56 +0530

    Update sender_stub.py

--- sender_stub.py
+++ sender_stub.py
@@ -4,4 +4,4 @@ def get_Temp_sensor_data():
   return random.randint(0,50) #rang is -20 to 50 degree celsius
 
 def get_Current_sensor_data():
-  return round(random.uniform(0,10), 2) #current sensing sensor output voltage in range of 0 to 10V
+  return round(random.uniform(0,5), 2) #current sensing sensor output voltage in range of 0 to 10V

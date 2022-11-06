b9c3c730b68580da831cfd3d97162764fb251a7e
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-06 23:44:57 +0530

    Update test_sender.py

--- test_sender.py
+++ test_sender.py
@@ -5,9 +5,20 @@ import sender_stub
 class sender_test(unittest.Testcase):
   
   def test_CelsiustoFarenheit(self):
-    self.assertEqual(sender.CelsiustoFarenheit(50),122.00)
-    self.assertEqual(sender.CelsiustoFarenheit(0),32.00)
-    self.assertEqual(sender.CelsiustoFarenheit(22),71.6)
+    self.assertEqual(sender.CelsiustoFarenheit(50),122.0,'0')
+    self.assertEqual(sender.CelsiustoFarenheit(0),32.0,'0')
+    self.assertEqual(sender.CelsiustoFarenheit(22),71.6,'0')
     
+  def test_A2D_conversion(self):
+    self.assertEqual(sender.A2D_conversion(0),0,'0')
+    self.assertEqual(sender.A2D_conversion(0.1),20,'0')
+    self.assertEqual(sender.A2D_conversion(2.5),512,'0')
+    self.assertEqual(sender.A2D_conversion(4.9),1003,'0')
+  
+  def test_Preprocess(self):
+    self.assertEqual(sender.Preprocess(0,0),(0,32.0),'0')
+    self.assertEqual(sender.Preprocess(1.2,32),(245,89.6),'0')
+    
+       
 if __name__ == '__main__':
   unittest.main()

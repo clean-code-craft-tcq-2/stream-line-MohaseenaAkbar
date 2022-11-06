c707f1e4a338d41a303e1eefbdd99e2442d75363
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-06 21:20:49 +0530

    Update test_sender.py

--- test_sender.py
+++ test_sender.py
@@ -1,10 +1,10 @@
-from  unittest import TestCase
+import unittest
 import sender
 import sender_stub
 
 class sender_test(unittest.Testcase):
   
-  def test_temp_A2D_conversion(self):
+  def test_CelsiustoFarenheit(self):
     self.assertEqual(CelsiustoFarenheit(50),122.00)
     self.assertEqual(CelsiustoFarenheit(0),32.00)
     self.assertEqual(CelsiustoFarenheit(-20),-4.00)

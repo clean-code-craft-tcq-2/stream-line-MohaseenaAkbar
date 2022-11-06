68c89b3387087e11c63c7552015b1c1fea75c81f
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-06 22:44:56 +0530

    Update test_sender.py

--- test_sender.py
+++ test_sender.py
@@ -7,7 +7,7 @@ class sender_test(unittest.Testcase):
   def test_CelsiustoFarenheit(self):
     self.assertEqual(sender.CelsiustoFarenheit(50),122.00)
     self.assertEqual(sender.CelsiustoFarenheit(0),32.00)
-    self.assertEqual(sender.CelsiustoFarenheit(-20),-4.00)
+    self.assertEqual(sender.CelsiustoFarenheit(22),71.6)
     
 if __name__ == '__main__':
   unittest.main()

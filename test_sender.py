a8a3dd1775a00cf4ac3e078eefc71c89b567084a
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-07 00:09:29 +0530

    Update test_sender.py

--- test_sender.py
+++ test_sender.py
@@ -18,7 +18,12 @@ class sender_test(unittest.TestCase):
   def test_Preprocess(self):
     self.assertEqual(sender.Preprocess(0,0),(0,32.0),'0')
     self.assertEqual(sender.Preprocess(1.2,32),(246,89.6),'0')
+  
+  def test_get_Temp_sensor_data(self):
+    self.assertTrue(sender_stub.get_Temp_sensor_data()<=50)
     
+  def test_get_Current_sensor_data(self):
+    self.assertTrue(sender_stub.get_Current_sensor_data()<=5.0)
        
 if __name__ == '__main__':
   unittest.main()

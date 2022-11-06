f7238b2ae6ee5fcfecd043a24fbcfbfb2116c326
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-06 20:12:51 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,26 +1,25 @@
 import sender_stub
 
-def temp_A2D_conversion(Analog_Volt):
-  TempInVolt=((Analog_Volt*1023)/5) #10 bit ADC and 5V system
-  TempInCelsius=TempInVolt*100
-  return TempInCelsius
+def A2D_conversion(raw_data):
+  return ((raw_data*5)/1024) #10 bit ADC and 5V system
 
 def CelsiustoFarenheit(TempInCelsius):
   return (((TempInCelsius*9)/5)+32)
 
-def temp_preprocess(rew_Temp_data):
-  Temp_celsius=temp_A2D_conversion(rew_Temp_data)
+def Preprocess(raw_data,Temp_celsius):
+  A2D_Out=A2D_conversion(raw_data)
   Temp_Farenheit=CelsiustoFarenheit(Temp_celsius)
-  return Temp_Farenheit
+  return A2D_Out,Temp_Farenheit
   
 def Sensor_reading_to_receiver(No_of_readings):
-  sensor_readings=[]
+  Sensor_readings=[]
   for i in range(No_of_readings)
-    rew_Temp_data=sender_stub.get_Temp_sensor_data()
-    Temp_Output=temp_preprocess(rew_Temp_data)
-    sensor_readings.append(Temp_Output)
-    print(Temp_Output)
-  return sensor_readings
+    Temp_data=sender_stub.get_Temp_sensor_data()
+    Output_Current=sender_stub.get_Current_sensor_data()
+    Temp_Output=Preprocess(Output_Current,Temp_data)
+    Sensor_readings.append(Output_CurrentTemp_Output)
+    print(Output_Current,Temp_Output)
+  return Sensor_readings
 
 if __name__ == '__main__':
   Sensor_reading_to_receiver(50)

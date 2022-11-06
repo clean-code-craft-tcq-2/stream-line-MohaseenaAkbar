4fa087e85540ebc208610dd9deca8ac479b08573
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-06 16:44:58 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
 import sender_stub
 
-def temp_A2D_conversion(rew_Temp_data):
-  TempInVolt=((rew_Temp_data/1024)*5)-0.5
+def temp_A2D_conversion(Analog_Volt):
+  TempInVolt=((Analog_Volt*1023)/5) #10 bit ADC and 5V system
   TempInCelsius=TempInVolt*100
   return TempInCelsius
 
@@ -13,11 +13,15 @@ def temp_preprocess(rew_Temp_data):
   Temp_Farenheit=CelsiustoFarenheit(Temp_celsius)
   return Temp_Farenheit
   
-def Sensor_reading_to_receiver():
-  rew_Temp_data=sender_stub.get_Temp_sensor_data()
-  Temp_Output=temp_preprocess(rew_Temp_data)
-  print(Temp_Output)
+def Sensor_reading_to_receiver(No_of_readings):
+  sensor_readings=[]
+  for i in range(No_of_readings)
+    rew_Temp_data=sender_stub.get_Temp_sensor_data()
+    Temp_Output=temp_preprocess(rew_Temp_data)
+    sensor_readings.append(Temp_Output)
+    print(Temp_Output)
+  return sensor_readings
 
 if __name__ == '__main__':
-  Sensor_reading_to_receiver()
+  Sensor_reading_to_receiver(50)
   

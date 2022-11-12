32363bf7ab999577e52c1dc7558bc87745b4ff56
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-12 14:54:54 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -4,6 +4,7 @@ def read_data_from_console():
   return receiver_stub.Console_output()
 
 def Find_Min_Max(readings):
-  return 2.5,50.1
+  return min(readings),max(readings)
 
 BMS_reading=read_data_from_console()
+Min_Value,Max_Value=Find_Min_Max(BMS_reading)

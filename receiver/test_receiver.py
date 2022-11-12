f8c6dd0883b4103043fa5dccaadcbe9d3ab4dc6a
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-12 14:44:00 +0530

    Update test_receiver.py

--- receiver/test_receiver.py
+++ receiver/test_receiver.py
@@ -2,7 +2,6 @@ import unittest
 import receiver
 
 class receiver_test(unittest.TestCase):
-
    
   def test_read_data_from_console(self):
     self.assertTrue(len(receiver.read_data_from_console())==100)
@@ -12,4 +11,7 @@ class receiver_test(unittest.TestCase):
     Min_Value,Max_Value=receiver.Find_Min_Max(Readings)
     self.assertTrue(Min_Value==2.5)
     self.assertTrue(Max_Value==50.1)
+   
+if __name__ == '__main__':
+  unittest.main()
     

58342bd9c88a47df7c48724e23cbd7f91da28cf6
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-12 14:26:29 +0530

    Update test_receiver.py

--- receiver/test_receiver.py
+++ receiver/test_receiver.py
@@ -5,5 +5,18 @@ class receiver_test(unittest.TestCase):
 
    
   def test_read_data_from_console(self):
-    self.assertTrue(receiver.read_data_from_console(50)!=0)
+    self.assertTrue(receiver.len(read_data_from_console())==100)
+      
+  def test_Find_Min_Max(self):
+    Min_val_expected = readings[0]
+    Max_val_expected = readings[0]
+    for val in readings:
+        if val < Min_val_expected:
+            Min_val_expected = val
+    for value in readings:
+        if value < Max_val_expected:
+            Max_val_expected = value
+    Min_Value_received,Max_Value_received=receiver.test_Find_Min_Max()
+    self.assertTrue(Min_Value_received==Min_val_expected)
+    self.assertTrue(Max_Value_received==Max_val_expected)
     

a81433a13fffd129b655d16f559e3170bd7d14c0
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-12 15:56:10 +0530

    Update receiver.py

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -6,5 +6,8 @@ def read_data_from_console():
 def Find_Min_Max(readings):
   return min(readings),max(readings)
 
+def Simple_Moving_Average(readings):
+  return 16.26
+
 BMS_reading=read_data_from_console()
 Min_Value,Max_Value=Find_Min_Max(BMS_reading)

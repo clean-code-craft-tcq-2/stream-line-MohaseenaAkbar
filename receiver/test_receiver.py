3a2db1f61340ca821eb07cb6392db7023cbab533
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-11-12 14:37:39 +0530

    Update test_receiver.py

--- receiver/test_receiver.py
+++ receiver/test_receiver.py
@@ -9,7 +9,7 @@ class receiver_test(unittest.TestCase):
       
   def test_Find_Min_Max(self):
     Readings=[2.5,8,10,50,49.9,50.1]
-    Min_Value,Max_Value=receiver.test_Find_Min_Max(Readings)
+    Min_Value,Max_Value=receiver.Find_Min_Max(Readings)
     self.assertTrue(Min_Value==2.5)
     self.assertTrue(Max_Value==50.1)
     
